46652b10acd5e3b63d406217ad3de3efea239465
sameer man shrestha <144588798+shresthasameerman@users.noreply.github.com>
2024-12-22 14:39:40 +0545

    Update ui.py

--- ui.py
+++ ui.py
@@ -19,7 +19,7 @@ def show_error(error):
 try:
     # Create the main window
     root = tk.Tk()
-    root.title("Samaaye Events")
+    root.title("Samaaye Interactives")
     root.geometry("1200x900")  # Adjust initial window size
     root.configure(bg=ACCENT_COLOR)
     root.update_idletasks()  # Ensure proper window initialization
